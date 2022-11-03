caeba7c1491e950ad54a8bc0f1aae7ed8c11c88e
Troels F. Rønnow <troels@wonop.com>
2022-11-03 17:07:11 +0100

    Attempting to get Docusaurus to work with Bazel

--- contract-examples/scilla-boilerplate/dist/utils/template.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ejs = require("ejs");
-function render(content, data) {
-    return ejs.render(content, data);
-}
-exports.render = render;
-//# sourceMappingURL=template.js.map
